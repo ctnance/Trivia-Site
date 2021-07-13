e6b9803a831ab73d3e1d01c1e6d4fd585aec52cd
Christopher Nance <ctnance0@gmail.com>
2021-07-13 14:57:49 -0400

    api content displaying

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@
 const questionsContainter = document.querySelector(".questions");
 
 // QUIZ VARIABLES
+let questionCount = 10;
+let difficulty = ["easy", "medium", "hard"];
 let questions = [];
 let multipleChoice = true;
 
 const fetchTriviaData = async () => {
   // Fetch response from API
   let response = await fetch(
-    `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`
+    `https://opentdb.com/api.php?amount=${questionCount}&category=15&difficulty=${
+      difficulty[1]
+    }&type=${multipleChoice ? "multiple" : "boolean"}`
   );
 
   // Ensure response was successful before proceeding
@@ -17,7 +21,8 @@ const fetchTriviaData = async () => {
     let data = await response.json();
     // Add retrieved questions from data and assign them to the array of questions
     questions = parseTriviaData(data);
-    console.log(questions);
+    createQuestion();
+    // console.log(questions);
   }
 };
 
@@ -31,3 +36,61 @@ const parseTriviaData = (data) => {
   });
   return questionArray;
 };
+
+const createQuestion = () => {
+  // Loop through each question object
+  console.log(questions);
+  questions.forEach((object, index) => {
+    // Create the container for the question
+    let questionContainer = document.createElement("div");
+    let questionID = index;
+    questionContainer.classList.add("question-container");
+    questionContainer.id = `${questionID}`;
+
+    // Create the label for the question label
+    let questionLabel = document.createElement("p");
+    questionLabel.innerHTML = `Question ${questionID + 1}`;
+    questionContainer.appendChild(questionLabel);
+
+    // Create the label for the question text
+    let questionText = document.createElement("p");
+    questionText.innerHTML = `${object.question}`;
+    questionContainer.appendChild(questionText);
+
+    questionContainer.appendChild(
+      createAnswers([object["correct_answer"], ...object["incorrect_answers"]])
+    );
+
+    questionsContainter.appendChild(questionContainer);
+  });
+};
+
+const createAnswers = (answers) => {
+  // Create unordered list of answers
+  let answersList = document.createElement("ul");
+  answersList.classList.add("answers");
+
+  answers.forEach((answer, index) => {
+    // Create item for answer
+    let item = document.createElement("li");
+
+    // Create the radio input
+    let input = document.createElement("input");
+    input.type = "radio";
+    input.name = `Answers${index}`;
+    input.id = answer;
+    input.value = answer;
+    item.appendChild(input);
+
+    // Create the label tag
+    let label = document.createElement("label");
+    label.for = answer;
+    label.innerHTML = answer;
+    item.appendChild(label);
+
+    answersList.appendChild(item);
+  });
+  return answersList;
+};
+
+fetchTriviaData();
